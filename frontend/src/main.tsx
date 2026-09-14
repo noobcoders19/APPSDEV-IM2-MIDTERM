3d24a14d5264ce9271e87e6656155f0213219a8c
noobcoders19 <nunezjohnclydee@gmail.comm>
2026-09-14 22:03:28 +0800

    Simplify frontend entrypoint and styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 import './global.css';
 import { AuthProvider } from './context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>,
 );
